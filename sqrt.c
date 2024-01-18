7582b49967a0ad2b08b1b6b1f3b9572b85fbb011
itzabhitri <itzabhitri@gmail.com>
2024-01-18 13:32:04 +0530

    added & modified some files

---
+++ sqrt.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+  double A = sqrt(9);
+  double B = pow(2,4);
+  printf("%.2lf\n", A);
+  printf("%.2lf\n", B);
+  return 0;
+}
--- userInput.c
+++ userInput.c
@@ -6,6 +6,7 @@ int main(){
   printf("What is your name?\n");
   // scanf("%s", &name);  Does not count white spaces.
   fgets(name, 25, stdin);  // Counts white spaces too.
+  name[strlen(name)-1]= '\0'; //to get new line character.
   printf("Hey %s, how old are you?\n", name);
   scanf("%d", &age);
   printf("Hey %s, you are %d years old.", name, age);
